2888a6313da1aa6e7e5a220fdb0438c3b6104cce
omarsar <ellfae@gmail.com>
2016-04-11 00:06:25 +0800

    added front end input box for username

--- app.rb
+++ app.rb
@@ -1,21 +1,30 @@
 require 'sinatra'
 require 'json'
 require 'haml'
+require 'httparty'
+require './config/environment'
+require 'bootstrap'
 
 # Visualization API
 class DeepViz < Sinatra::Base
+  
+  API_URL = ENV['API_URL']
+
   # home url
-  get '/?' do    
+  get '/?' do
+    @api_url = API_URL     
     haml :index
   end
 
   # home for bpd users
-  get '/bpd'
-    
+  get '/bpd' do
+    @user_name = params['username']
+    @user_data = HTTParty.get(API_URL + "predict_json?screen_name=#{@user_name}")
+    haml :bpd    
   end
 
   # home for bp users
-  get '/bp'
+  get '/bp' do
   
   end
 
