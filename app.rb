a7838891e982a3480df29447aac707db46508b7b
rjollet- <renaudjollet@gmail.com>
2016-05-01 19:19:11 +0800

    Update the view and app
    Creat a view for user and one for patient
    Chenge the index to search or see an example
    change the look of the dashboard

--- app.rb
+++ app.rb
@@ -28,92 +28,92 @@ class DeepViz < Sinatra::Base
     @user_profile = @user_data['profile']
     @user_report = @user_data['report']
 
-    # array for dimensions of depression
-    @dep_dimensions = [
-      @user_report['BPD_probability'],
-      @user_report['bipolar_probability'],
-      #@user_report['negative_combos'],
-      #@user_report['positive_combos'],
-      #@user_report['first_pronoun_ratio'],
-      @user_report['mentioning_frequency'],
-      #@user_report['tweeting_frequency'],
-      #@user_report['flips_ratio'],
-      @user_report['negative_ratio'],
+    @tweets_polarity = [
       @user_report['positive_ratio'],
+      1 - @user_report['negative_ratio'] - @user_report['positive_ratio'],
+      @user_report['negative_ratio'],
     ]
+
+    tweetParsing = Proc.new{ |tweet|
+        {
+          x: 0,
+          low: Time.parse(tweet['time']).to_i,
+          high: Time.parse(tweet['time']).to_i + (tweet['dt'].to_i * 100),
+          text: tweet['text']
+        }
+      }
+
+    @positive_tweets = @user_report['positive_tweets'].map(&tweetParsing).to_json
+    @negative_tweets = @user_report['negative_tweets'].map(&tweetParsing).to_json
 
     @bpd_word_count = bpd_word_count(@user_data['BPD_words'])
-    # ==============================================================================
-    # information about MD patient
-    # TODO: obtain this from a database
-    #@random_patient_name = "BarelyNerdy"
-    #@random_patient_name = "bipolar_type_II"
-    if params['case'] == "bipolar"
-      @random_patient_name = get_bipolar()
-    else
-      @random_patient_name = get_bpd()
-      #@random_patient_name = "OneCalicoSky"
-    end
-
-    @random_patient_data = HTTParty.get(API_URL + "predict_json_by_id?user_id=#{@random_patient_name}")
-
-    # separate the data into two pieces
-    @random_patient_profile = @random_patient_data['profile']
-    @random_patient_report = @random_patient_data['report']
-
-    @random_patient_dep_dimensions = [
-      @random_patient_report['BPD_probability'],
-      @random_patient_report['bipolar_probability'],
-      #@random_patient_report['negative_combos'],
-      #@random_patient_report['positive_combos'],
-      #@user_report['first_pronoun_ratio'],
-      @random_patient_report['mentioning_frequency'],
-      #@random_patient_report['tweeting_frequency'],
-      #@random_patient_report['flips_ratio'],
-      @random_patient_report['negative_ratio'],
-      @random_patient_report['positive_ratio'],
-    ]
-
-    @random_patient_bpd_word_count = bpd_word_count(@random_patient_data['BPD_words'])
-
-    @overlap_information = overlap_words(@user_data['BPD_words'], @random_patient_data['BPD_words'])
-
-    @overlap_words = @overlap_information[0]
-    @overlap_normal_count = @overlap_information[1]
-    @overlap_patient_count = @overlap_information[2]
-
-    haml :compare
+    haml :user
   end
 
   # home for bpd users
   get '/bpd' do
-    @user_name = params['username']
-    @user_data = HTTParty.get(API_URL + "predict_json?screen_name=#{@user_name}")
+    # information about queried user
+    @random_patient_name = get_bpd()
+    @user_data = HTTParty.get(API_URL + "predict_json_by_id?user_id=#{@random_patient_name}")
 
     # separate the data into two pieces
     @user_profile = @user_data['profile']
     @user_report = @user_data['report']
 
-    # array for dimensions of depression
-    @dep_dimensions = [
-      @user_report['BPD_probability'],
-      @user_report['bipolar_probability'],
-      @user_report['negative_combos'],
-      @user_report['positive_combos'],
-      #@user_report['first_pronoun_ratio'],
-      @user_report['flips_ratio'],
-      @user_report['negative_ratio'],
+    @tweets_polarity = [
       @user_report['positive_ratio'],
+      1 - @user_report['negative_ratio'] - @user_report['positive_ratio'],
+      @user_report['negative_ratio'],
     ]
+
+    tweetParsing = Proc.new{ |tweet|
+        {
+          x: 0,
+          low: Time.parse(tweet['time']).to_i,
+          high: Time.parse(tweet['time']).to_i + (tweet['dt'].to_i * 10),
+          text: tweet['text']
+        }
+      }
+
+    @positive_tweets = @user_report['positive_tweets'].map(&tweetParsing).to_json
+    @negative_tweets = @user_report['negative_tweets'].map(&tweetParsing).to_json
 
     @bpd_word_count = bpd_word_count(@user_data['BPD_words'])
 
-    haml :bpd
+    haml :patient
   end
 
   # home for bp users
   get '/bd' do
+    # information about queried user
+    @random_patient_name = get_bipolar()
+    @user_data = HTTParty.get(API_URL + "predict_json_by_id?user_id=#{@random_patient_name}")
 
+    # separate the data into two pieces
+    @user_profile = @user_data['profile']
+    @user_report = @user_data['report']
+
+    @tweets_polarity = [
+      @user_report['positive_ratio'],
+      1 - @user_report['negative_ratio'] - @user_report['positive_ratio'],
+      @user_report['negative_ratio'],
+    ]
+
+    tweetParsing = Proc.new{ |tweet|
+        {
+          x: 0,
+          low: Time.parse(tweet['time']).to_i,
+          high: Time.parse(tweet['time']).to_i + (tweet['dt'].to_i * 10),
+          text: tweet['text']
+        }
+      }
+
+    @positive_tweets = @user_report['positive_tweets'].map(&tweetParsing).to_json
+    @negative_tweets = @user_report['negative_tweets'].map(&tweetParsing).to_json
+
+    @bpd_word_count = bpd_word_count(@user_data['BPD_words'])
+
+    haml :patient
   end
 
 end
