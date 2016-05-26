efe367a523529056a86d5b26647901b6d309981c
omarsar <ellfae@gmail.com>
2016-05-26 18:24:57 +0800

    fixed api bug

--- app.rb
+++ app.rb
@@ -9,33 +9,19 @@ require './config/environment'
 class DeepViz < Sinatra::Base
   helpers Helpers
 
-  # decide the API URL
-  def self.setapi(master, slave)
-    default = master
-    begin
-      response = HTTP.post(default)
-      if response.code == 200
-        return master
-      else
-        return slave
-      end
-    rescue
-      return slave
-    end
-  end
-
-  API_URL = setapi(ENV['API_URL_1'],ENV['API_URL_2'])
+  
+  #API_URL = 
 
   # home url
   get '/?' do
-    @api_url = API_URL
+    @api_url = setapi(ENV['API_URL_1'],ENV['API_URL_2'])
     haml :index
   end
 
   # home for comparison dashbaord
   get '/compare' do
     begin
-      @api_url = API_URL
+      @api_url = setapi(ENV['API_URL_1'],ENV['API_URL_2'])
       # information about queried user
       @sample1_data = getUserData(params['sample1radio'], params['username1'], @api_url)
       @sample2_data = getUserData(params['sample2radio'], params['username2'], @api_url)
